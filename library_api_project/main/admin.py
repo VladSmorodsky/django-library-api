5e3d88c2f33f314fe7380e5c21772d4387c85ddb
Vlad <vlad.zah@ukr.net>
2025-03-24 08:59:29 +0200

    DLA-2 Changed models structure

--- library_api_project/main/admin.py
+++ library_api_project/main/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 
-from main.models import Book, Author, Genre
+from main.models import Book
 
 # Register your models here.
-admin.site.register(Author)
-admin.site.register(Genre)
 admin.site.register(Book)
