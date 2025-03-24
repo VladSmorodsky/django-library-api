e241cac69bb7139d7c1e265aafe554ce96709f02
Vlad <vlad.zah@ukr.net>
2025-03-24 08:54:17 +0200

    DLA-2 Created models

--- library_api_project/main/models.py
+++ library_api_project/main/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Author(models.Model):
+    """
+    Author model
+    """
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
+
+class Genre(models.Model):
+    """
+    Genre model
+    """
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
+
+class Book(models.Model):
+    """
+    Book model
+    """
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, blank=True, null=True, on_delete=models.DO_NOTHING)
+    publication_year = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'{self.author.__str__()} - {self.title}'
